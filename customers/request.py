cd4156f7e61e015cdfa6fc23e9c1678c50b80663
emilycentko <emily.centko@gmail.com>
2021-04-08 12:18:37 -0500

    Single and all customers

---
+++ customers/request.py
@@ -0,0 +1,52 @@
+CUSTOMERS = [
+     {
+      "id": 1,
+      "name": "Jean-Luc Picard",
+      "address": "1701 Enterprise Ct.",
+      "email": "jlp@enterprise.co"
+    },
+    {
+      "id": 2,
+      "name": "Dana Scully",
+      "address": "100 Believe Way",
+      "email": "d.scully@skeptic.com"
+    },
+    {
+      "id": 3,
+      "name": "Peter Parker",
+      "address": "200 Chicago St.",
+      "email": "peter.parker@imnotspiderman.com"
+    },
+    {
+      "id": 4,
+      "name": "Mina Harker",
+      "address": "190 Transylvania Ave.",
+      "email": "mina.harker@dracula.com"
+    },
+    {
+      "id": 5,
+      "name": "Han Solo",
+      "address": "77 Millennium Way",
+      "email": "han@rebels.org"
+    },
+    {
+      "email": "f.mulder@believer.com",
+      "name": "Fox Mulder",
+      "address": "360 Aliens Drive",
+      "id": 6
+    }
+]
+
+def get_all_customers():
+    return CUSTOMERS
+
+
+def get_single_customer(id):
+    requested_customer = None
+
+    for customer in CUSTOMERS:
+    
+        if customer["id"] == id:
+            requested_customer = customer
+
+    return requested_customer
