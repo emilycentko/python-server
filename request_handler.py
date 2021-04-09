62ef85da3145bd6609ed138a96652a1c2fca0f0f
emilycentko <emily.centko@gmail.com>
2021-04-09 12:22:00 -0500

    Able to delete animal

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 import json
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, create_animal
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal
 from locations import get_all_locations, get_single_location, create_location
 from employees import get_all_employees, get_single_employee, create_employee
 from customers import get_all_customers, get_single_customer, create_customer
@@ -139,6 +139,20 @@ class HandleRequests(BaseHTTPRequestHandler):
             pass  # Request had trailing slash: /animals/
 
         return (resource, id)  # This is a tuple
+    
+    def do_DELETE(self):
+        # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
 
 # This function is not inside the class. It is the starting
