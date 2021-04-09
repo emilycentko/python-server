d131f7857d4431fed610ab2d7bf4ae3ea56c504b
emilycentko <emily.centko@gmail.com>
2021-04-09 12:31:49 -0500

    Able to delete an employee

--- request_handler.py
+++ request_handler.py
@@ -3,7 +3,7 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal
 from locations import get_all_locations, get_single_location, create_location, delete_location
-from employees import get_all_employees, get_single_employee, create_employee
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee
 from customers import get_all_customers, get_single_customer, create_customer
 
 
@@ -155,6 +155,11 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         if resource == "locations":
             delete_location(id)
+
+        self.wfile.write("".encode())
+
+        if resource == "employees":
+            delete_employee(id)
 
         self.wfile.write("".encode())
 
