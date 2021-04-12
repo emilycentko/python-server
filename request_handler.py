c76abecc4f87fe21743fcd312e8bb81a383f0032
emilycentko <emily.centko@gmail.com>
2021-04-12 09:28:38 -0500

    Update animal function and successfully tested PUT request in Postman

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 import json
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, create_animal, delete_animal
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 from locations import get_all_locations, get_single_location, create_location, delete_location
 from employees import get_all_employees, get_single_employee, create_employee, delete_employee
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer
@@ -117,7 +117,20 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
     def do_PUT(self):
-        self.do_POST()
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
     
     def parse_url(self, path):
         # Just like splitting a string in JavaScript. If the
@@ -139,6 +152,7 @@ class HandleRequests(BaseHTTPRequestHandler):
             pass  # Request had trailing slash: /animals/
 
         return (resource, id)  # This is a tuple
+
     
     def do_DELETE(self):
         # Set a 204 response code
