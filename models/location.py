f2c672602b4f8e70f117542a56066683d69654e0
emilycentko <emily.centko@gmail.com>
2021-04-12 10:39:16 -0500

    Created classes per module in models directory

---
+++ models/location.py
@@ -0,0 +1,7 @@
+class Location():
+
+    def __init__(self, id, name, address, volunteers):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.address = volunteers
