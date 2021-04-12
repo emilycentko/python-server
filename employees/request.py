81b8f1746a9936ac203edaa668f963502c692abe
emilycentko <emily.centko@gmail.com>
2021-04-12 10:05:45 -0500

    Update employees function and tested PUT request

--- employees/request.py
+++ employees/request.py
@@ -2,22 +2,26 @@ EMPLOYEES = [
     {
       "id": 1,
       "name": "Rose Nylund",
-      "locationId": 2
+      "locationId": 2,
+      "role": "Front desk"
     },
     {
       "id": 2,
       "name": "Sophia Petrillo",
-      "locationId": 1
+      "locationId": 1,
+      "role": "Manager"
     },
     {
       "id": 3,
       "name": "Dorothy Zbornak",
-      "locationId": 1
+      "locationId": 1,
+      "role": "Caretaker"
     },
     {
       "id": 4,
       "name": "Blanche Devereaux",
-      "locationId": 3
+      "locationId": 3,
+      "role": "Caretaker"
     }
 ]
 
@@ -56,3 +60,9 @@ def delete_employee(id):
 
     if employee_index >= 0:
         EMPLOYEES.pop(employee_index)
+
+def update_employee(id, new_employee):
+    for index, employee in enumerate(EMPLOYEES):
+        if employee["id"] == id:
+            EMPLOYEES[index] = new_employee
+            break
--- request_handler.py
+++ request_handler.py
@@ -3,7 +3,7 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 from locations import get_all_locations, get_single_location, create_location, delete_location, update_location
-from employees import get_all_employees, get_single_employee, create_employee, delete_employee
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer
 
 
@@ -134,6 +134,11 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         if resource == "locations":
             update_location(id, post_body)
+
+        self.wfile.write("".encode())
+
+        if resource == "employees":
+            update_employee(id, post_body)
 
         self.wfile.write("".encode())
     
