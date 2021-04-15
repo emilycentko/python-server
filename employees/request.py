0d66e6d06644f56034346b795caa50a980146da0
emilycentko <emily.centko@gmail.com>
2021-04-15 10:18:32 -0500

    Able to query employees by location id

--- employees/request.py
+++ employees/request.py
@@ -100,3 +100,28 @@ def update_employee(id, new_employee):
         if employee["id"] == id:
             EMPLOYEES[index] = new_employee
             break
+
+def get_employees_by_location(location_id):
+
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        select
+            e.id,
+            e.name,
+            e.location_id
+        from Employee e
+        WHERE e.location_id = ?
+        """, ( location_id, ))
+
+        employees = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            employee = Employee(row['id'], row['name'], row['location_id'])
+            employees.append(employee.__dict__)
+
+    return json.dumps(employees)
--- request_handler.py
+++ request_handler.py
@@ -3,7 +3,7 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal, get_animals_by_location
 from locations import get_all_locations, get_single_location, create_location, delete_location, update_location
-from employees import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee, get_employees_by_location
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer, get_customers_by_email
 
 
@@ -69,6 +69,9 @@ class HandleRequests(BaseHTTPRequestHandler):
 
             if key == "location_id" and resource == "animals":
                 response = get_animals_by_location(value)
+            
+            if key == "location_id" and resource == "employees":
+                response = get_employees_by_location(value)
 
         self.wfile.write(response.encode())
 
